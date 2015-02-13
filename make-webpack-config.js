8853747abcecd3a81bf753c6bfae5ae5059d6aa6
Tobias Koppers <tobias.koppers@googlemail.com>
2015-02-13 18:05:41 +0100

    enabled hash for css

--- make-webpack-config.js
+++ make-webpack-config.js
@@ -109,7 +109,7 @@ module.exports = function(options) {
 		}
 	});
 	if(options.separateStylesheet && !options.prerender) {
-		plugins.push(new ExtractTextPlugin("[name].css"));
+		plugins.push(new ExtractTextPlugin("[name].css" + (options.longTermCaching ? "?[chunkhash]" : "")));
 	}
 	if(options.minimize) {
 		plugins.push(
