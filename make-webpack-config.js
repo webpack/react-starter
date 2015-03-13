0b0e2ca798c6bfb7f2761d274d1deb8217175067
FoxxMD <matt.duncan13@gmail.com>
2015-03-13 11:09:39 -0400

    Run all js files in app through babel-loader

--- make-webpack-config.js
+++ make-webpack-config.js
@@ -13,10 +13,11 @@ module.exports = function(options) {
 		"coffee": "coffee-redux-loader",
 		"jsx": options.hotComponents ? ["react-hot-loader", "babel-loader"] : "babel-loader",
 		"json": "json-loader",
-		// "js": {
-			// loader: "6to5-loader",
-			// include: path.join(__dirname, "app")
-		// },
+		"js": {
+			loader: "babel-loader",
+			include: path.join(__dirname, "app"),
+            exclude: /node_modules/
+		},
 		"json5": "json5-loader",
 		"txt": "raw-loader",
 		"png|jpg|jpeg|gif|svg": "url-loader?limit=10000",
