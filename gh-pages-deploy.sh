fbf35e4ceda73ec5a79bce9c0b8e4054e376be91
Joseph Finlayson <joseph.finlayson@gmail.com>
2015-06-18 23:37:42 +0200

    copying files out of public folder

--- gh-pages-deploy.sh
+++ gh-pages-deploy.sh
@@ -5,6 +5,6 @@ echo $randomNum
 cp lib/staticSite.html build/public/index.html;
 git branch gh-pages;
 git checkout gh-pages;
-cp -r build/public .
+cp -r build/public/* .
 git add .;
 git commit -m "Automated commit on ${commitDate}";
